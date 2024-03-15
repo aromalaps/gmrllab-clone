6d07657b3548fc023c32e2f346fcb0ca8ff7a279
aromalprasad <aromalaps@gmail.com>
2024-03-15 22:27:51 +0530

    portfolio

---
+++ frontend gmrllab/hospital frtond end project/pract/page6/main6_carousel.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $('.main6_package-scroll').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1280,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 770,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    centerPadding: '0px',
+
+                }
+            }
+        ]
+    });
+});
